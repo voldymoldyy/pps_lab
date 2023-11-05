b0ecbb192d905bca55dccf8488d921089f0133ad
perplexity <135210710+voldymoldyy@users.noreply.github.com>
2023-11-05 16:41:34 +0530

    update

---
+++ Btech AI&DS B1/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    char c[100];
+    std::cout << "enter char: ";
+    std::cin.getline(c, 100);
+
+    char *ptr = &c[0];
+    int count = 0;
+    for (int i = 0; *(ptr + i) != '\0'; i++)
+    {
+        count++;
+    }
+    std::cout << "length of string: " << count;
+}
---
+++ Btech AI&DS B1/2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cout << "enter rows: ";
+    std::cin >> n;
+
+    int m;
+    std::cout << "enter columns: ";
+    std::cin >> m;
+
+    char arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            std::cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            arr[i][j] = '$';
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            std::cout << arr[i][j];
+            std::cout << "\t";
+        }
+        std::cout << "\n";
+    }
+}
